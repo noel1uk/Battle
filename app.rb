376be9e31eb94e572f78cef2f802494ff3517545
Laura Chan <laurachan205@gmail.com>
2018-02-06 17:12:47 +0000

    Add 'session' to gameplay

--- app.rb
+++ app.rb
@@ -1,16 +1,28 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     # 'Testing infrastructure working!'
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
+    # @player1 = params[:Player1]
+    # @player2 = params[:Player2]
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb(:play)
   end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/enter_players_spec.rb
+++ spec/features/enter_players_spec.rb
@@ -4,14 +4,14 @@ feature "name input" do
 
   scenario "has a form" do
     visit('/')
-    expect(page).to have_field(:Player1)
+    expect(page).to have_field(:player1)
     expect(page).to have_selector("form")
   end
 
-  scenario "player 1's name is displayed" do
+  scenario "player 1 and 2's names are displayed" do
     visit('/')
-    fill_in 'Player1', with: 'July'
-    fill_in 'Player2', with: 'Jill'
+    fill_in 'player1', with: 'July'
+    fill_in 'player2', with: 'Jill'
     click_button 'Submit'
     expect(page).to have_content("Welcome July and Jill")
   end
